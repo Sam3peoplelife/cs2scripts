35af0bb918b6ea7bf48eb8dc0b1e50a59a06efbf
Sam3peoplelife <83673324+Sam3peoplelife@users.noreply.github.com>
2025-06-22 04:00:37 +0200

    init

---
+++ autoReportScript.py
@@ -0,0 +1,23 @@
+import pyautogui
+import keyboard
+import time
+
+while True:
+    if keyboard.is_pressed('k'):
+        for _ in range(10):
+            pyautogui.click()
+            x0, y0 = pyautogui.position()
+
+            pyautogui.moveTo(x0 + 350, y0 + 100)
+            time.sleep(0.2)
+            pyautogui.click()
+
+            pyautogui.moveTo(950,540)
+            time.sleep(0.2)
+            pyautogui.click()
+
+            pyautogui.moveTo(930, 710)
+            pyautogui.click()
+            
+            pyautogui.moveTo(x0, y0)
+            time.sleep(0.1)
